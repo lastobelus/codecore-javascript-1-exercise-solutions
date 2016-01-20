8efdafe37f6f38a216ffbf3731aa5eed240460a2
Michael Johnston <lastobelus@mac.com>
2016-01-20 14:24:06 -0800

    demo 01: prompt and alert name
    slide 45

--- demo-01-name.js
+++ demo-01-name.js
@@ -1,8 +1,7 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-  var name = "Michael Johnston";
-  var a = 3, b = 4, result = a * b;
-  var message = "Hello, " + name + "! In case you forgot, " + a + " x " + b + " is " + result + "."
-  console.log(message);
+  var name = prompt("What...is your name?");
+  var message = "Hello " + name + "!";
+  alert(message);
   var nameDiv = document.getElementById('name');
   nameDiv.innerHTML = message;
 });
