e10808b7b4228ee3979a4d057aa9a842caa13a2c
Michael Johnston <lastobelus@mac.com>
2016-01-20 14:36:22 -0800

    demo 01: friendly safe
    slide 58

--- demo-01-safe.js
+++ demo-01-safe.js
@@ -1,10 +1,16 @@
 var safe = 714;
 var password = "opensesame";
+var entranceMessage = "Welcome to super-safe! Are you sure you want to enter?";
 
-var passwordAttempt = prompt("What is the password?");
+if (confirm(entranceMessage)) {
+  var passwordAttempt = prompt("What is the password?");
 
-if (passwordAttempt === password) {
-  alert("the safe contains " + safe);
+  if (passwordAttempt === password) {
+    alert("the safe contains " + safe);
+  } else {
+    alert("sorry, wrong password");
+  }
 } else {
-  alert("sorry, wrong password");
+  alert("OK. Goodbye, then.");
 }
+
