03c12a6499925a7b941f716ebb7bcfb381503c8d
Michael Johnston <lastobelus@mac.com>
2016-01-20 14:21:15 -0800

    demo 01: write message to console and document
    using variables. Slide 43

--- demo-01-name.js
+++ demo-01-name.js
@@ -1,4 +1,8 @@
 document.addEventListener("DOMContentLoaded", function(event) {
+  var name = "Michael Johnston";
+  var a = 3, b = 4, result = a * b;
+  var message = "Hello, " + name + "! In case you forgot, " + a + " x " + b + " is " + result + "."
+  console.log(message);
   var nameDiv = document.getElementById('name');
-  nameDiv.innerHTML = "Michael Johnston";
+  nameDiv.innerHTML = message;
 });
