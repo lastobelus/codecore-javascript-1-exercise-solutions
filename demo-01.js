cea1af84a62cb81a5b313ed13aadd8c15fc203a4
Michael Johnston <lastobelus@mac.com>
2016-01-19 20:35:32 -0800

    demo 01: external file

---
+++ demo-01.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+  var outputDiv = document.getElementById('output');
+  var now = new Date();
+  outputDiv.innerHTML = "Current Date & Time: " + now;
+});
